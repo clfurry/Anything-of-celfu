f7360ad54c37cbd3462e5e1d6eb7fba82510b976
Maximiliano <owoowic@gmail.com>
2025-07-25 23:23:20 -0400

    Actualizar celfu_tests.js

--- celfu_tests.js
+++ celfu_tests.js
@@ -1,4 +1,17 @@
 // testing mod
+elements.prionic_ice = {
+    color: "#A1B298",
+    behavior: behaviors.GAS,
+    category: "states",
+    state: "gas",
+    hidden = true,
+    tempHigh = -70,
+    tempLow = -500,
+    stateHigh = "prionic_gas",
+    stateLow = "",
+    reactions = {},
+    density = 1300
+};
 elements.prionic_gas = {
     color: "#6C9066",
     behavior: behaviors.GAS,
@@ -6,7 +19,7 @@ elements.prionic_gas = {
     state: "gas",
     hidden = false,
     tempHigh = 1000,
-    tempLow = -30,
+    tempLow = -80,
     stateHigh = "ash",
     stateLow = "prionic_ice",
     reactions = {"body": { elem1: "prionic_gas", elem2: "prionic_gas"},
