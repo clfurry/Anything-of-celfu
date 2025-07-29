06bd2ef7837a4597cbb28c7454bef333e932695b
Maximiliano <owoowic@gmail.com>
2025-07-29 17:26:02 -0400

    Update celfu_tests.js

--- celfu_tests.js
+++ celfu_tests.js
@@ -1,34 +1,45 @@
 // testing mod
+elements.aminoacids = {
+    color: "#dbc5d3",
+    behavior: behaviors.POWDER,
+    category: "states",
+    state: "solid",
+    hidden: true,
+    reactions: {},
+    density: 1000,
+    viscosity: 10000
+};
 elements.prionicIce = {
     color: "#a1b298",
-    behavior: behaviors.GAS,
+    behavior: behaviors.WALL,
     category: "states",
-    state: "gas",
-    hidden = true,
-    tempHigh = -70,
-    tempLow = -200,
-    stateHigh = "prionic_gas",
-    stateLow = "prionic_ice",
-    reactions = {},
-    density = 1300,
-    viscosity = 30000000000000
+    state: "solid",
+    hidden: true,
+    tempHigh: -70,
+    tempLow: 200,
+    stateHigh: "prionicGas",
+    stateLow: "prionicIce",
+    reactions: {},
+    density: 1300,
+    viscosity: 30000000000000,
+    breakInto: "aminoacids"
 };
 elements.prionicGas = {
     color: "#6c9066",
     behavior: behaviors.GAS,
-    category: "Gases",
+    category: "gases",
     state: "gas",
-    hidden = false,
-    tempHigh = 1000,
-    tempLow = -80,
-    stateHigh = "ash",
-    stateLow = "prionic_ice",
-    reactions = {
-"body": { elem1: "prionic_gas", elem2: "prionic_gas"},
-        {"head": { elem1: "prionic_gas", elem2: "prionic_gas"}}
-    },
-    density = 0.00058,
-    viscosity = 0.1,
-    alpha = 0.5,
-    glow = true
+    hidden: false,
+    tempHigh: 1000,
+    tempLow: -80,
+    stateHigh: "ash",
+    stateLow: "prionicIce",
+    density: 0.058,
+    viscosity: 5,
+    alpha: 0.5,
+    glow: true,
+    reactions: {
+    "body": { elem1: "prionicGas", elem2: "prionicGas"},
+    "head": { elem1: "prionicGas", elem2: "prionicGas"}
+}
 };
